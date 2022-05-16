22ff51d9909037aab76b227abb3558a9664c0ad1
deibydev <deiby.morisaki@gmail.com>
2022-05-16 14:54:02 -0500

    frontend finalizado

---
+++ Front/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDDlhxAr1QLu3EAeXN_EqZDqE5lozhUx74",
+    authDomain: "sofkau-57ab4.firebaseapp.com",
+    projectId: "sofkau-57ab4",
+    storageBucket: "sofkau-57ab4.appspot.com",
+    messagingSenderId: "800377516073",
+    appId: "1:800377516073:web:88406777f4ef2b1c29ac38",
+    measurementId: "G-MX6F56NV8R"
+  }
+
+};
---
+++ Front/src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDDlhxAr1QLu3EAeXN_EqZDqE5lozhUx74",
+    authDomain: "sofkau-57ab4.firebaseapp.com",
+    projectId: "sofkau-57ab4",
+    storageBucket: "sofkau-57ab4.appspot.com",
+    messagingSenderId: "800377516073",
+    appId: "1:800377516073:web:88406777f4ef2b1c29ac38",
+    measurementId: "G-MX6F56NV8R"
+  }
+
+  
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
